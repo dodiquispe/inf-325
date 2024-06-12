e70ea070446bf0cb63c905f15b936d63c4eda371
dodiquispe <juditcoyoquispe@gmail.com>
2024-06-12 03:58:37 -0400

    hola

---
+++ lib/snake_game_page.dart
@@ -0,0 +1,236 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+class SnakeGamePage extends StatefulWidget {
+  const SnakeGamePage({super.key});
+
+  @override
+  State<SnakeGamePage> createState() => _SnakeGamePageState();
+}
+
+enum Direction { up, down, left, right }
+
+class _SnakeGamePageState extends State<SnakeGamePage> {
+  int row = 20, column = 20;
+  List<int> borderList = [];
+  List<int> snakePosition = [];
+  int snakeHead = 0;
+  int score = 11;
+  late Direction direction;
+  late int foodPoistion;
+  late int foodPoistiona;
+  late int foodPoistionb;
+
+  @override
+  void initState() {
+    startGame();
+    super.initState();
+  }
+
+  void startGame() {
+    makeBorder();
+    generateFood();
+    generateFooda();
+    direction = Direction.right;
+    snakePosition = [45, 44, 43];
+    snakeHead = snakePosition.first;
+    Timer.periodic(const Duration(milliseconds: 300), (timer) {
+      updateSnake();
+      if (checkCollision()) {
+        timer.cancel();
+        showGameOverDialog();
+      }
+    });
+  }
+
+  void showGameOverDialog() {
+    showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("FIN"),
+          content: const Text("Tu serpiente chocó!"),
+          actions: [
+            TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  startGame();
+                },
+                child: const Text("Volver a Jugar"))
+          ],
+        );
+      },
+    );
+  }
+
+  bool checkCollision() {
+    //if snake collid with border
+    if (borderList.contains(snakeHead)) return true;
+    //if snake collid with itself
+    if (snakePosition.sublist(1).contains(snakeHead)) return true;
+    return false;
+  }
+
+  void generateFood() {//funcion para la comida aparece
+    foodPoistion = Random().nextInt(row * column);
+    if (borderList.contains(foodPoistion)) {
+      generateFood();
+
+    }
+  }
+  void generateFooda() {//funcion para la comida aparece
+    foodPoistiona = Random().nextInt(row * column);
+    if (borderList.contains(foodPoistiona)) {
+      generateFooda();
+
+    }
+  }
+
+  void updateSnake() {
+    setState(() {
+      switch (direction) {
+        case Direction.up:
+          snakePosition.insert(0, snakeHead - column);
+          break;
+        case Direction.down:
+          snakePosition.insert(0, snakeHead + column);
+          break;
+        case Direction.right:
+          snakePosition.insert(0, snakeHead + 1);
+          break;
+        case Direction.left:
+          snakePosition.insert(0, snakeHead - 1);
+          break;
+      }
+    });
+
+    if (snakeHead == foodPoistion) {
+      score++;
+      generateFood();
+      generateFooda();
+    } else {
+      snakePosition.removeLast();
+    }
+
+    snakeHead = snakePosition.first;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [Expanded(child: _buildGameView()), _buildGameControls()],
+      ),
+    );
+  }
+
+  Widget _buildGameView() {
+    return GridView.builder(
+      gridDelegate:
+          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: column),
+      itemBuilder: (context, index) {
+        return Container(
+          margin: const EdgeInsets.all(1),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: fillBoxColor(index)),
+        );
+      },
+      itemCount: row * column,
+    );
+  }
+
+  Widget _buildGameControls() {
+    return Container(
+      padding: const EdgeInsets.all(20),
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Punaje : $score"),
+          IconButton(
+            onPressed: () {
+              if (direction != Direction.down) direction = Direction.up;
+            },
+            icon: const Icon(Icons.arrow_circle_up),
+            iconSize: 100,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                onPressed: () {
+                  if (direction != Direction.right) direction = Direction.left;
+                },
+                icon: const Icon(Icons.arrow_circle_left_outlined),
+                iconSize: 100,
+              ),
+              const SizedBox(width: 100),
+              IconButton(
+                onPressed: () {
+                  if (direction != Direction.left) direction = Direction.right;
+                },
+                icon: const Icon(Icons.arrow_circle_right_outlined),
+                iconSize: 100,
+              ),
+            ],
+          ),
+          IconButton(
+            onPressed: () {
+              if (direction != Direction.up) direction = Direction.down;
+            },
+            icon: const Icon(Icons.arrow_circle_down_outlined),
+            iconSize: 100,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Color fillBoxColor(int index) {
+    if (borderList.contains(index))
+      return Color.fromARGB(255, 17, 2, 124);
+    else {
+      if (snakePosition.contains(index)) {
+        if (snakeHead == index) {
+          return Color.fromARGB(255, 70, 3, 101);
+        } else {
+          return Color.fromARGB(255, 148, 103, 103).withOpacity(0.9);
+        }
+      } else {
+        if (index == foodPoistion) {
+          return getRandomColor();
+         // return Colors.red;
+        }
+      }
+    }
+    return Color.fromARGB(255, 236, 137, 202).withOpacity(0.05);
+  }
+  // Función para generar un color aleatorio
+Color getRandomColor() {
+  Random random = Random();
+  return Color.fromARGB(
+    255, // Opacidad máxima
+    random.nextInt(256), // Valor rojo
+    random.nextInt(256), // Valor verde
+    random.nextInt(256), // Valor azul
+  );
+}
+  makeBorder() {
+    for (int i = 0; i < column; i++) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
+    for (int i = 0; i < row * column; i = i + column) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
+    for (int i = column - 1; i < row * column; i = i + column) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
+    for (int i = (row * column) - column; i < row * column; i = i + 1) {
+      if (!borderList.contains(i)) borderList.add(i);
+    }
+  }
+}
